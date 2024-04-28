fadd5e1b3262ccc27ade47a18a0c730cfd92b7bc
MUKIL S <124667264+mukil-subramaniam@users.noreply.github.com>
2024-04-28 22:21:28 +0530

    s

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const panoramaImage = new PANOLENS.ImagePanorama("https://as2.ftcdn.net/v2/jpg/01/32/29/01/1000_F_132290171_BB7dc95933jnmExd4j0XvfQYy4N3wCUx.jpg");
+const panoramaImage = new PANOLENS.ImagePanorama("https://mukil-subramaniam.github.io/Images/IMG20240426172524.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
   controlBar: true,
-  enableVR: true,
+  enableVR: false,
 });
 
 viewer.add(panoramaImage);
